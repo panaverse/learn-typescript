27c85b08afb5651f12502dc79a816e6478d9f709
Zia Khan <ziaukhan@hotmail.com>
2015-06-02 20:32:11 +0500

    changed name of step2

---
+++ step2/app.js
@@ -0,0 +1,12 @@
+//strongly typed syntax
+var a = "Pakistan";
+a = "USA";
+var b = 9;
+var c = true;
+//type inference
+var e = "USA";
+var f = 10.9;
+f = 22;
+var g = false;
+g = true;
+//# sourceMappingURL=app.js.map
---
+++ step2/readme.txt
@@ -0,0 +1,2 @@
+Read typing syntax in chapter 2 of Mastering TypeScript Book:
+http://www.amazon.com/Mastering-TypeScript-Nathan-Rozentals/dp/1784399663/
