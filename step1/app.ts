c1fdfe23dfea555281d2b87dbe6a28555cf82ee2
Zia Khan <ziaukhan@hotmail.com>
2015-06-02 20:18:05 +0500

    added step 1

---
+++ step1/app.ts
@@ -0,0 +1,15 @@
+//strongly typed syntax
+var a : string = "Pakistan";
+a = "USA";
+var b : number = 9;
+var c : boolean = true;
+
+//type inference
+var e = "USA";
+var f = 10.9;
+f = 22;
+var g = false;
+g = true;
+
+
+
