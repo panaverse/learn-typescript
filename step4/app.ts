341b9f6cd609b307ea62c3ffbd969065e175f29a
Zia Khan <ziaukhan@hotmail.com>
2015-06-02 23:04:49 +0500

    added step4

---
+++ step4/app.ts
@@ -0,0 +1,5 @@
+let array1 : number[] = [5, 6, 7];//correct syntax
+console.log(array1[1]);//correct syntax
+let array2:Array<number> = [1, 2, 3];//alternative correct syntax
+let array3 : number[] = [];//correct syntax to define an empty array
+
