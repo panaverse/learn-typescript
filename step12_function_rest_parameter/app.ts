42fdd7cf6829012e28244f41397c700b22851337
Zia Khan <ziaukhan@hotmail.com>
2015-06-03 18:48:25 +0500

    added rest parameters

---
+++ step12_function_rest_parameter/app.ts
@@ -0,0 +1,18 @@
+function buildName(firstName: string, ...restOfName: string[]) {//Named function with Rest parameters
+	return firstName + " " + restOfName.join(" ");
+}
+
+var employeeName = buildName("Joseph", "Samuel", "Lucas", "MacKinzie");
+
+
+
+
+//anonymous function type with Rest parameters
+var buildNameFun: (fname: string, ...rest: string[])=>string =
+function (firstName: string, ...restOfName: string[]) {
+	return firstName + " " + restOfName.join(" ");
+}
+
+//Note: Rest, optional and default parameters can only be at the end of the parameter list
+											
+						
