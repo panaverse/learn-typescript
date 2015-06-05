0819be99b67a58d7ccca99fa38a3a33c6888cf44
Zia Khan <ziaukhan@hotmail.com>
2015-06-05 15:01:25 +0500

    added accessors

---
+++ step24_accessors/app.ts
@@ -0,0 +1,32 @@
+//TypeScript supports getters/setters as a way of intercepting accesses to a member of an object, thus allowing Encapsulation
+//This gives you a way of having finer-grained control over how a member is accessed on each object.
+
+//Encapsulation is the packing of data and functions into a single component. 
+//It allows selective hiding of properties and methods in an object by building an impenetrable wall to protect the code from accidental corruption.
+
+var passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+	
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            alert("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+var employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    alert(employee.fullName);
+}
+
+//We check to make sure the user has a secret passcode available before we allow them to modify the employee.
---
+++ step24_accessors/readme.txt
@@ -0,0 +1,3 @@
+Read:
+http://www.typescriptlang.org/Handbook#classes-accessors
+http://en.wikipedia.org/wiki/Encapsulation_%28object-oriented_programming%29
