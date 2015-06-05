ad5967da01c0072b8053ae5f670fb7c5157c87ed
Zia Khan <ziaukhan@hotmail.com>
2015-06-05 12:38:27 +0500

    added protected

---
+++ step23_classes_protected_modifier/app.ts
@@ -0,0 +1,29 @@
+//protected keyword allows subclasses to gain visibility into the parent class without exposing this API to other parts of the code.
+
+class ListString {
+    private contents: string[];
+    constructor() {
+        this.contents = [];
+    }
+    protected setElement(index: number, item: string) {
+        this.contents[index] = item;
+    }
+
+}
+
+class StackString extends ListString {
+    currentIndex: number;
+
+    constructor() {
+        super();
+        this.currentIndex = 0;
+    }
+
+    public push(item: string) {
+        this.setElement(this.currentIndex, item);
+        this.currentIndex++;
+    }
+}
+
+var stack = new StackString();
+//stack.setElement(0, 1); // error 'setElement' is protected and only visible to subclasses 
---
+++ step23_classes_protected_modifier/readme.txt
@@ -0,0 +1,2 @@
+Read:
+http://blogs.msdn.com/b/typescript/archive/2014/11/12/announcing-typescript-1-3.aspx
