e906a3562bba45ecccc67dcaa9b0fc8d7975d345
Zia Khan <ziaukhan@hotmail.com>
2015-10-01 10:16:08 +0500

    updated step 15

--- step15_function_overloads/app.js
+++ step15_function_overloads/app.js
@@ -1,11 +1,8 @@
-//overloads give us type-checked calls 
-//In the first example we have the same return type
+//overloads give us type-checked calls
 function add(arg1, arg2) {
     return arg1 + arg2;
 }
 //Calling 'add' with any other parameter types would cause an error except for the three options
-add(1, 2);
-add("Hello", "World");
-add(true, false);
-//In the example below we have different return types for different options 
-//# sourceMappingURL=app.js.map
+console.log(add(1, 2));
+console.log(add("Hello", "World"));
+console.log(add(true, false));
--- step15_function_overloads/app.ts
+++ step15_function_overloads/app.ts
@@ -4,7 +4,7 @@
 function add(arg1: string, arg2: string): string;//option 1
 function add(arg1: number, arg2: number): number;//option 2
 function add(arg1: boolean, arg2: boolean): boolean;//option 3
-function add(arg1: any, arg2: any): any {//this is not part of the overload list, so it only has three overloads
+function add(arg1: any, arg2: any): any {//this is not part of the overload list, so it has only three overloads
     return arg1 + arg2;
 }
 
