2157bd2c661533eae3b70200c8b9a8d7a1b24dde
Zia Khan <ziaukhan@hotmail.com>
2015-10-01 09:58:17 +0500

    updated step 13

--- step13_lambda/app.ts
+++ step13_lambda/app.ts
@@ -3,7 +3,12 @@ var add = (a: number, b: number) => {
 }
 
 var add1 = (x: number, y: number) => (x + y);
+//output var add1 = function(x, y){return x + y};
 
 var myFunction = f => { this.x = "x"; }//automatically creating the that-equals-this pattern
-											
+/*
+When compiled, TypeScript automatically makes sure this points to my class instead of the enclosed function:
+var _this = this;
+var myFunction = function (f) { _this.x = "x"; };
+*/											
 						
