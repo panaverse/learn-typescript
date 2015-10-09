6c0b9cad7648149fe15a6f6ba162d30ed5d9f1d2
Zia Khan <ziaukhan@hotmail.com>
2015-10-09 15:30:59 +0500

    updated step 20a

--- step20a_inheritance/app.js
+++ step20a_inheritance/app.js
@@ -97,3 +97,64 @@ var B = (function (_super) {
     return B;
 })(A);
 var obj = new B();
+//*******************************
+//checking types
+var Foo = (function () {
+    function Foo() {
+    }
+    return Foo;
+})();
+var Bar = (function (_super) {
+    __extends(Bar, _super);
+    function Bar() {
+        _super.apply(this, arguments);
+    }
+    return Bar;
+})(Foo);
+var Bas = (function () {
+    function Bas() {
+    }
+    return Bas;
+})();
+var bar = new Bar();
+console.log(bar instanceof Bar); // true
+console.log(bar instanceof Foo); // true
+console.log(bar instanceof Object); // true
+console.log(bar instanceof Bas); // false
+//**************************
+//Type Guards
+//http://blogs.msdn.com/b/typescript/archive/2014/11/18/what-s-new-in-the-typescript-type-system.aspx
+//Using instanceof with classes:
+/*
+class Dog { woof() { } }
+class Cat { meow() { } }
+var pet: Dog|Cat = ... ;
+if(pet instanceof Dog) {
+   pet.woof(); // OK
+} else {
+   pet.woof(); // Error
+}
+*/
+/*User defined type guards in 1.6
+
+In earlier versions of TypeScript, you could use if statements to narrow the type. For example, you could use:
+
+if (typeof x === "number") { … }
+
+This helped type information flow into common ways of working with types at runtime (inspired by some of the other projects doing typechecking of JS). While this approach is powerful, we wanted to push it a bit further.  In 1.6, you can now create your own type guard functions:
+
+interface Animal {name: string; }
+interface Cat extends Animal { meow(); }
+
+function isCat(a: Animal): a is Cat {
+  return a.name === 'kitty';
+}
+
+var x: Animal;
+
+if(isCat(x)) {
+  x.meow(); // OK, x is Cat in this block
+}
+
+This allows you to work with not only typeof and instanceof checks, which need a type that JavaScript understands, but now you can work with interfaces and do custom analysis.  Guard functions are denoted by their “a is X” return type, which returns boolean and signals to the compiler if what the expected type now is
+*/
