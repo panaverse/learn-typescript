1fb2d5e1963c019581860851decf69e64242b00e
Zia Khan <ziaukhan@hotmail.com>
2015-10-09 14:43:06 +0500

    updated step35 again

--- step35_modules/app.ts
+++ step35_modules/app.ts
@@ -24,7 +24,7 @@ console.log(h.name);
 //If it is in output js file you will have to use System.js to use it becuase right now most browsers and node.js do not support ES2015 modules. 
 
 
-/*ECMAScript 6 provides the following ways of importing [4]:
+/*ECMAScript 6 provides the following ways of importing:
 
     // Default exports and named exports
     import theDefault, { named1, named2 } from 'src/mylib';
@@ -39,4 +39,51 @@ console.log(h.name);
     import * as mylib from 'src/mylib';
     
     // Only load the module, don’t import anything
-    import 'src/mylib';*/
+    import 'src/mylib';
+	*/
+	
+	
+	
+	
+	/*
+	There are two ways in which you can export things that are inside the current module. 
+	On one hand, you can mark declarations with the keyword export.
+
+    export var myVar1 = ...;
+    export let myVar2 = ...;
+    export const MY_CONST = ...;
+    
+    export function myFunc() {
+        ...
+    }
+    export function* myGeneratorFunc() {
+        ...
+    }
+    export class MyClass {
+        ...
+    }
+The “operand” of a default export is an expression (including function expressions and class expressions). Examples:
+
+    export default 123;
+    export default function (x) {
+        return x
+    };
+    export default x => x;
+    export default class {
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+    };
+On the other hand, you can list everything you want to export at the end of the module (which is once again similar in style to the revealing module pattern).
+
+    const MY_CONST = ...;
+    function myFunc() {
+        ...
+    }
+    
+    export { MY_CONST, myFunc };
+You can also export things under different names:
+
+    export { MY_CONST as THE_CONST, myFunc as theFunc };
+	*/
