c8702bbabd893d927148ea14a16394caa5da3003
Zeeshan Hanif <zeeshanhanif@gmail.com>
2015-10-20 20:52:38 +0500

    updated step 24

--- step24_accessors/app.js
+++ step24_accessors/app.js
@@ -1,7 +1,8 @@
 //TypeScript supports getters/setters as a way of intercepting accesses to a member of an object, thus allowing Encapsulation
 //This gives you a way of having finer-grained control over how a member is accessed on each object.
 //Encapsulation is the packing of data and functions into a single component. 
-//It allows selective hiding of properties and methods in an object by building an impenetrable wall to protect the code from accidental corruption.
+//It allows selective hiding of properties and methods in an object by building an impenetrable wall to protect the code
+// from accidental corruption.
 //Note getters and setters are only supported when targeting ES 5 or higher
 var passcode = "secret passcode";
 var Employee = (function () {
@@ -29,4 +30,25 @@ employee.fullName = "Bob Smith";
 if (employee.fullName) {
     console.log(employee.fullName);
 }
-//We check to make sure the user has a secret passcode available before we allow them to modify the employee. 
+//We check to make sure the user has a secret passcode available before we allow them to modify the employee.
+// Case:2 -- For any case if we want to apply access modifier other then public on getter and setter then we must apply same access modifier on both getter and setter
+var Human = (function () {
+    function Human() {
+    }
+    Object.defineProperty(Human.prototype, "fullName", {
+        get: function () {
+            return this._fullName;
+        },
+        set: function (newName) {
+            if (passcode && passcode == "secret passcode") {
+                this._fullName = newName;
+            }
+            else {
+                console.log("Error: Unauthorized update of employee!");
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Human;
+})();
--- step24_accessors/app.ts
+++ step24_accessors/app.ts
@@ -2,7 +2,8 @@
 //This gives you a way of having finer-grained control over how a member is accessed on each object.
 
 //Encapsulation is the packing of data and functions into a single component. 
-//It allows selective hiding of properties and methods in an object by building an impenetrable wall to protect the code from accidental corruption.
+//It allows selective hiding of properties and methods in an object by building an impenetrable wall to protect the code
+// from accidental corruption.
 
 //Note getters and setters are only supported when targeting ES 5 or higher
 
@@ -32,3 +33,23 @@ if (employee.fullName) {
 }
 
 //We check to make sure the user has a secret passcode available before we allow them to modify the employee.
+
+
+// Case:2 -- For any case if we want to apply access modifier other then public on getter and setter then we must apply same access modifier on both getter and setter
+class Human {
+    private _fullName: string;
+
+    
+    protected get fullName(): string {
+        return this._fullName;
+    }
+	
+    protected set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            console.log("Error: Unauthorized update of employee!");
+        }
+    }
+}
