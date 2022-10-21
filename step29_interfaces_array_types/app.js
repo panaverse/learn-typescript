015eea1baa5bcd12b5761e3c35452d874287caac
Zia Khan <ziaukhan@hotmail.com>
2022-10-21 10:35:45 +0500

    updated steps 21-30

--- step29_interfaces_array_types/app.js
+++
@@ -1,6 +0,0 @@
-//We can use interfaces to describe an array also, please note this is different from Java or C# 
-//where we can only use interfaces to describe class types.
-var myArray = ["Bob", "Fred"];
-var first = myArray[0];
-var myDictionary = { "first": "Bob", "second": "Fred" };
-var first = myDictionary["first"];
