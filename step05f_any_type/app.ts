dda3b4f34eb615b2f1e9b917024a4e8239c5bf5d
Zia Khan <ziaukhan@hotmail.com>
2022-11-15 21:06:19 +0500

    updated numbering

--- step05f_any_type/app.ts
+++ step05f_any_type/app.ts
@@ -1,6 +1,6 @@
 let myType : any = { name: "Zia", id: 1 };
-myType = { id: 2,  name: "Tom" };// can only assign a type which has the at least the same properties
-myType = { id: 3,  name: "Mike", gender: false };//becuase of any it assign a different type
+myType = { id: 2, name: "Tom" };
+myType = { id: 3,  name: "Mike", gender: false };//because of any it assign a different type
 myType = { name: "Mike", gender: false };//can even reduce the properties because of any type
 
 myType = "Even a sring can be assigned";
