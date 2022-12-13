1420bfeeaa81bfcad6550daf00bdf4ebd5223d3f
Zia Khan <ziaukhan@hotmail.com>
2022-12-13 18:11:33 +0500

    update 17

--- step17_accessors/app.ts
+++ step17_accessors/app.ts
@@ -1,28 +1,25 @@
-//TypeScript supports getters/setters as a way of intercepting accesses to a member of an object, thus allowing Encapsulation
-//This gives you a way of having finer-grained control over how a member is accessed on each object.
+//TypeScript supports getters/setters as a way of intercepting 
+//accesses to a member of an object, thus allowing Encapsulation
+//This gives you a way of having finer-grained control over how a 
+//member is accessed on each object.
 
-//Encapsulation is the packing of data and functions into a single component. 
-//It allows selective hiding of properties and methods in an object by building an impenetrable wall to protect the code
+//Encapsulation is the packing of data and functions into a single 
+//component. 
+//It allows selective hiding of properties and methods in an 
+//object by building an impenetrable wall to protect the code
 // from accidental corruption.
-
-//Note getters and setters are only supported when targeting ES 5 or higher
 
 var passcode = "secret passcode";
 
 class Employee {
-    private _fullName: string;
+    private _fullName: string | undefined;
 
-    get fullName(): string {
+    get fullName(): string | undefined {
         return this._fullName;
     }
 	
-    set fullName(newName: string) {
-        if (passcode && passcode == "secret passcode") {
-            this._fullName = newName;
-        }
-        else {
-            console.log("Error: Unauthorized update of employee!");
-        }
+    set fullName(newName: string | undefined) {
+        this._fullName = newName;
     }
 }
 
@@ -32,24 +29,3 @@ if (employee.fullName) {
     console.log(employee.fullName);
 }
 
-//We check to make sure the user has a secret passcode available before we allow them to modify the employee.
-
-
-// Case:2 -- For any case if we want to apply access modifier other then public on getter and setter then we must apply same access modifier on both getter and setter
-class Human {
-    private _fullName: string;
-
-    
-    protected get fullName(): string {
-        return this._fullName;
-    }
-	
-    protected set fullName(newName: string) {
-        if (passcode && passcode == "secret passcode") {
-            this._fullName = newName;
-        }
-        else {
-            console.log("Error: Unauthorized update of employee!");
-        }
-    }
-}
