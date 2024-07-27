b7a678c4ab58b18546b6595df3848bf2e994a42a
Fazilat Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2024-07-27 22:51:53 +0500

    Update app.ts

--- step00c_type_error/app.ts
+++ step00c_type_error/app.ts
@@ -1,3 +1,7 @@
-let message = "Hello World";
-console.loger(message);
-        
+// A type error in TypeScript occurs when a value is used in a way that is incompatible with its declared type.
+
+let myName: string = "unknown person"
+myName = 4 // Here, try to assign the number 4 to variable `myName`. Since `myName` is type string, TypeScript will throw a type error: Type 'number' is not assignable to type 'string'.
+
+console.log(myName);
+
